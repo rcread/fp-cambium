ac7055cc7d1b747e1a52a9b28b3cdf5427790f6e
rcread <rachelread@thecambiumgroup.co.uk>
2022-09-22 08:50:37 +0100

    contact page update

--- js/main.js
+++ js/main.js
@@ -15,6 +15,8 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
+
+// hamburger menu functionality
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -28,19 +30,3 @@ document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click",
   navMenu.classList.remove("active");
 }))
 
-// hamburger menu functionality
-// function openSideMenu(){
-//   document.getElementById('menu-display').classList.add('show-menu');
-// }
-
-// function closeSideMenu(){
-//   document.getElementById('menu-display').classList.remove('show-menu');
-// }
-
-// document.getElementById('menu-trigger').addEventListener('click', closeSideMenu);
-// document.getElementById('close-menu').addEventListener('click', openSideMenu);
-
-// const menuControl = document.getElementById('menu-control');
-// menuControl.addEventListener('click', function(){
-//   document.querySelector('.menu').classList.toggle('menu-open');
-// });
